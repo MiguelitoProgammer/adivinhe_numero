d1e4cfebaea2ccfe27ff43c08017b01873249bb3
MiguelitoDeV <miguel0lima6@gmail.com>
2025-08-13 00:50:50 -0300

    Upando finalizado

--- AdivinheNumeroProjeto/AdivinheNumero.java
+++ AdivinheNumeroProjeto/AdivinheNumero.java
@@ -1,5 +1,34 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class AdivinheNumero {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int numeroSecreto = random.nextInt(100) + 1; // numero entre 1 a 100 será gerado
+        int tentativas = 0;
+        int palpite;
+
+        System.out.println("Bem vindo ao jogo de adivinhar o número!");
+        System.out.println("Tente adivinhar o número de 1 a 100.");
+
+        do {
+            System.out.println("Digite seu palpite: ");
+            palpite = scanner.nextInt();
+            tentativas++;
+
+            if (palpite > numeroSecreto) {
+                System.out.println("Muito alto! Tente outro numéro.");
+            } else if (palpite < numeroSecreto) {
+                System.out.println("Muito baixo! Tente outro número.");
+            } else {
+                System.out.println("Paraéns! Você acertou o número secreto em " + tentativas + " tentativas.");
+            }
+        } while (palpite != numeroSecreto);
+
+        scanner.close();
+
 
     }
 }
